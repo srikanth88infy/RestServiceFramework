c1c75e3dbfc1fffff5bb414e29a28a58bbc78c3d
Dominik Dorn <dominik@dominikdorn.com>
2010-03-21 17:37:22 +0100

    jpa placement dao + restful annotation

---
+++ src/main/java/com/dominikdorn/tuwien/evs/rest/annotations/Restful.java
@@ -0,0 +1,9 @@
+package com.dominikdorn.tuwien.evs.rest.annotations;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public interface Restful {
+}
---
+++ src/main/java/com/dominikdorn/tuwien/evs/rest/dao/JpaPlacementDao.java
@@ -0,0 +1,11 @@
+package com.dominikdorn.tuwien.evs.rest.dao;
+
+import com.dominikdorn.tuwien.evs.rest.domain.Placement;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public class JpaPlacementDao extends AbstractJpaDao<Placement> implements PlacementDao {
+}
