1592b2bf5a4908f90608268a55832b144778ed5d
Dominik Dorn <dominik@dominikdorn.com>
2010-03-21 17:35:45 +0100

    created abstract placementdao test

---
+++ src/test/java/com/dominikdorn/tuwien/evs/rest/dao/AbstractPlacementDaoTest.java
@@ -0,0 +1,203 @@
+package com.dominikdorn.tuwien.evs.rest.dao;
+
+import com.dominikdorn.tuwien.evs.rest.domain.Item;
+import com.dominikdorn.tuwien.evs.rest.domain.Placement;
+import com.dominikdorn.tuwien.evs.rest.domain.Rack;
+import org.junit.Test;
+
+import javax.persistence.PersistenceException;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public abstract class AbstractPlacementDaoTest {
+
+    protected PlacementDao dao; 
+
+    protected ItemDao itemDao;
+    protected RackDao rackDao;
+
+    final Item item_item1 = new Item(
+            "1HE Intel Atom Single-CPU CSE502 Server",
+            "1HE Intel Atom Single-CPU CSE502 Server",
+            1
+    );
+
+    final Item item_item2 = new Item(
+            "1HE Intel Single-CPU SC811 Server (Nehalem) ",
+            "1HE Intel Single-CPU SC811 Server (Nehalem) ",
+            1
+    );
+
+    final Item item_item3 = new Item(
+            200l,
+            "1HE Intel Single-CPU SC813M Server (Nehalem) ",
+            "1HE Intel Single-CPU SC813M Server (Nehalem) ",
+            1
+    );
+
+    final Item item_item4 = new Item(
+            "1HE Intel Atom D510 Single-CPU CSE502 Server ",
+            "1HE Intel Atom D510 Single-CPU CSE502 Server", 1);
+
+    final Rack rack_item1 = new Rack(
+            "HP Rack 10636 G2 Shock Pallet Rack",
+            "Rack 19\" 36U w/d/h 24in/39.8in/68.5in ",
+            36
+    );
+    final Rack rack_item2 = new Rack(
+            "Belkin Premium Rack ",
+            "Rack 19\" 42U w/d/h 24in/42.1in/79.9in ",
+            42
+    );
+
+    final Rack rack_item3 = new Rack(
+            200l,
+            "HP Rack 10842 G2 Wide Rack Cabinet Shock Pallet Rack",
+            "Rack 19\" 42U w/d/h 31.5in/39.8in/78.7in ",
+            42
+    );
+    final Rack rack_item4 = new Rack(
+            "APC NetShelter SX Enclosure with Sides Rack",
+            "Rack 19\" 42U w/d/h 23.6in/42.1in/78.3in ",
+            42
+    );
+
+    final Placement item1 = new Placement(item_item1, rack_item1, 5, "bla");
+    final Placement item2 = new Placement(item_item2, rack_item2, 10, "blub");
+    final Placement item3 = new Placement(15l, item_item2, rack_item2, 10, "bling");
+    final Placement item4 = new Placement(item_item4, rack_item4, 3, "boing");
+
+    @Test(expected = PersistenceException.class)
+    public void testPersist_shouldThrowExceptionWhenNullParameter() {
+        dao.persist(null);
+    }
+
+    @Test
+    public void testPersist_shouldWork() {
+        itemDao.persist(item_item1);
+        rackDao.persist(rack_item1);
+        Placement newItem = dao.persist(item1);
+
+        assertNotNull(newItem);
+        assertTrue(newItem.getId() > 0);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void testGetById_nullId() {
+        dao.getById(null);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void testGetById_negativeId_shouldThrowException() {
+        dao.getById(-1l);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void testGetById_zeroId_shouldThrowException() {
+        dao.getById(0l);
+    }
+
+    @Test
+    public void testGetById_shouldWorkWithPrePersistedEntity() {
+        itemDao.persist(item_item2);
+        rackDao.persist(rack_item2);
+        Placement newItem = dao.persist(item2);
+
+        assertNotNull(newItem);
+        assertTrue(newItem.getId() > 0);
+
+        Placement resultItem = dao.getById(newItem.getId());
+        assertNotNull(resultItem);
+        assertEquals(newItem.getId(), resultItem.getId());
+        assertEquals(newItem.getStoringPosition(), resultItem.getStoringPosition());
+        assertEquals(newItem.getAmount(), resultItem.getAmount());
+    }
+
+
+    @Test
+    public void testGetById_shouldFailWithNonExistingEntity() {
+        // no persist here!
+        Placement resultItem = dao.getById(item3.getId());
+        assertNull(resultItem);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void testUpdate_shouldThrowExceptionWhenNullParameter() {
+        dao.persist(null);
+    }
+
+    @Test
+    public void testUpdate_shouldWork() {
+        Placement newItem = dao.persist(item4);
+
+        assertNotNull(newItem);
+        assertTrue(newItem.getId() > 0);
+
+        newItem.setAmount(5);
+
+        Placement newItem2 = dao.update(newItem);
+        assertNotNull(newItem2);
+        assertEquals(5, (int) newItem2.getAmount());
+    }
+
+    @Test
+    public void testGetAll_shouldReturnNothing() {
+        List<Placement> results = dao.getAll();
+        assertNotNull(results);
+        assertTrue(results.isEmpty());
+    }
+
+    @Test
+    public void testGetAll_shouldFindItems() {
+        itemDao.persist(item_item1);
+        itemDao.persist(item_item2);
+        itemDao.persist(item_item3);
+        itemDao.persist(item_item4);
+
+        rackDao.persist(rack_item1);
+        rackDao.persist(rack_item2);
+        rackDao.persist(rack_item3);
+        rackDao.persist(rack_item4);
+
+        dao.persist(item1);
+        dao.persist(item2);
+        dao.persist(item3);
+        List<Placement> results = dao.getAll();
+        assertNotNull(results);
+        assertEquals(3, results.size());
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void testDelete_nullParam_shouldThrowException() {
+        dao.delete(null);
+        fail();
+    }
+
+    @Test
+    public void testDelete_shouldWorkCorrectly() {
+        List<Placement> items = dao.getAll();
+        assertTrue(items.isEmpty());
+
+        itemDao.persist(item_item1);
+        rackDao.persist(rack_item1);       
+
+        dao.persist(item1);
+
+        items = dao.getAll();
+        assertEquals(1, items.size());
+
+        dao.delete(item1);
+
+        items = dao.getAll();
+        assertTrue(items.isEmpty());
+    }
+
+
+}
