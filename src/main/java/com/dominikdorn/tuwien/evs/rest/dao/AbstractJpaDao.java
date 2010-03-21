c53c4e5d3af833d07ebd97b850b6019295e9fe12
Dominik Dorn <dominik@dominikdorn.com>
2010-03-21 17:10:20 +0100

    created abstract dao

---
+++ src/main/java/com/dominikdorn/tuwien/evs/rest/dao/AbstractJpaDao.java
@@ -0,0 +1,72 @@
+package com.dominikdorn.tuwien.evs.rest.dao;
+
+import com.dominikdorn.tuwien.evs.rest.domain.Item;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public class AbstractJpaDao<TYPE> {
+    @PersistenceContext
+    protected EntityManager em;
+
+    protected Class entityClass;
+
+    public AbstractJpaDao() {
+        ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
+        this.entityClass = (Class<TYPE>) genericSuperclass.getActualTypeArguments()[0];
+    }
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+
+    public TYPE persist(TYPE item) {
+        if (item == null)
+            throw new PersistenceException("Item may not be null");
+        em.persist(item);
+        return item;
+    }
+
+    public TYPE update(TYPE item) {
+        if (item == null)
+            throw new PersistenceException("Item may not be null");
+
+        em.merge(item);
+        return item;
+    }
+
+    public List<TYPE> getAll() {
+        CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return em.createQuery(cq).getResultList();       
+    }
+
+    public TYPE getById(Long id) {
+        if (id == null || id < 1)
+            throw new PersistenceException("Id may not be null or negative");
+
+        return (TYPE) em.find(entityClass, id);
+    }
+
+    public void delete(TYPE item) {
+        if (item == null)
+            throw new PersistenceException("Item may not be null");
+
+        em.remove(em.merge(item));
+    }
+}
