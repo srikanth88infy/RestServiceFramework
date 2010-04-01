c667855e3b5a5d56e72434a6bb183912423a4e1d
Dominik Dorn <dominik@dominikdorn.com>
2010-04-01 21:33:14 +0200

    created enum for possible output types

---
+++ src/main/java/com/dominikdorn/rest/services/OutputType.java
@@ -0,0 +1,13 @@
+package com.dominikdorn.rest.services;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public enum OutputType {    
+    UNSUPPORTED,
+    TEXT,
+    JSON,
+    XML
+}
