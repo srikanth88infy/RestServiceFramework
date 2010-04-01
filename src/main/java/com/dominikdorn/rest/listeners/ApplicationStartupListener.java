9f7e452c1d25025ab801a7f45740cf3588ac79d6
Dominik Dorn <dominik@dominikdorn.com>
2010-04-01 21:39:03 +0200

    adjusted import statements in applicationStartupListener

---
+++ src/main/java/com/dominikdorn/rest/listeners/ApplicationStartupListener.java
@@ -0,0 +1,92 @@
+package com.dominikdorn.rest.listeners;
+
+import com.dominikdorn.rest.invoking.Invoker;
+import com.dominikdorn.rest.marshalling.JsonMarshallingStrategy;
+import com.dominikdorn.rest.marshalling.Marshaller;
+import com.dominikdorn.rest.marshalling.MarshallingStrategy;
+import com.dominikdorn.rest.marshalling.XmlMarshallingStrategy;
+import com.dominikdorn.rest.services.AnnotationScanner;
+import com.dominikdorn.rest.services.ObjectRegistry;
+import com.dominikdorn.rest.services.OutputType;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public class ApplicationStartupListener implements ServletContextListener {
+
+
+
+    /**
+     * * Notification that the web application initialization
+     * * process is starting.
+     * * All ServletContextListeners are notified of context
+     * * initialization before any filter or servlet in the web
+     * * application is initialized.
+     */
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        AnnotationScanner scanner = new AnnotationScanner();
+        ObjectRegistry registry = new ObjectRegistry();
+
+        registry.setObjectDatabase(scanner.getRegisteredObjectMap(getWebInfClassesURL(sce.getServletContext())));
+
+        // creating marshalling-strategies
+        JsonMarshallingStrategy jsonMarshallingStrategy = new JsonMarshallingStrategy();
+        XmlMarshallingStrategy xmlMarshallingStrategy = new XmlMarshallingStrategy();
+
+        Map<OutputType, MarshallingStrategy> strategies = new HashMap<OutputType, MarshallingStrategy>();
+        strategies.put(OutputType.JSON, jsonMarshallingStrategy);
+        strategies.put(OutputType.XML, xmlMarshallingStrategy);
+
+
+        // creating marshaller
+        Marshaller marshaller = new Marshaller(strategies, OutputType.JSON);
+
+        Invoker invoker = new Invoker(marshaller);
+
+
+        // registers the registry & the invoker in application scope
+        sce.getServletContext().setAttribute("restObjectRegistry", registry);
+        sce.getServletContext().setAttribute("restInvoker", invoker);
+    }
+
+    public URL getWebInfClassesURL(ServletContext context) {
+        Set objs = context.getResourcePaths("/WEB-INF/classes/");
+
+        for (Object o : objs) {
+            try {
+                URL url = context.getResource(o.toString());
+                String urlString = url.toString();
+                int index = urlString.lastIndexOf("/WEB-INF/classes/");
+                urlString = urlString.substring(0, index + "/WEB-INF/classes/".length());
+                return new URL(urlString);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * * Notification that the servlet context is about to be shut down.
+     * * All requestHandler and filters have been destroy()ed before any
+     * * ServletContextListeners are notified of context
+     * * destruction.
+     */
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
