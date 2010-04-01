b68b56d0c92923f8df768bcd37858485de593528
Dominik Dorn <dominik@dominikdorn.com>
2010-04-01 21:43:41 +0200

    added restful annotation to placement

--- src/main/java/com/dominikdorn/tuwien/evs/rest/domain/Placement.java
+++ src/main/java/com/dominikdorn/tuwien/evs/rest/domain/Placement.java
@@ -1,5 +1,7 @@
 package com.dominikdorn.tuwien.evs.rest.domain;
 
+import com.dominikdorn.tuwien.evs.rest.annotations.Restful;
+
 import javax.persistence.*;
 
 /**
@@ -11,6 +13,7 @@ import javax.persistence.*;
  * to a certain Rack. One has to
  */
 @Entity
+@Restful("placements")
 public class Placement {
 
     @Id
