5a9d6fbf37dbabc1ab79d349589bed3d0a216186
Dominik Dorn <dominik@dominikdorn.com>
2010-04-02 02:48:44 +0200

    added an operationType enum

---
+++ src/main/java/com/dominikdorn/rest/requestHandling/OperationType.java
@@ -0,0 +1,22 @@
+package com.dominikdorn.rest.requestHandling;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public enum OperationType {
+
+    INVALID_TYPE,
+
+    BAD_REQUEST,
+
+    SPECIFIC_ID,
+    SEARCH,
+
+    ONLY_RESOURCE,
+
+
+
+
+}
