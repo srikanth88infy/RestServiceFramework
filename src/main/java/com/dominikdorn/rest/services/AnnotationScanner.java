64b18a6fcc7c1ec8ea68aa4938b4bba8ee51fb8b
Dominik Dorn <dominik@dominikdorn.com>
2010-04-09 01:09:23 +0200

    AnnotationScanner now also registers converters

--- src/main/java/com/dominikdorn/rest/services/AnnotationScanner.java
+++ src/main/java/com/dominikdorn/rest/services/AnnotationScanner.java
@@ -1,7 +1,9 @@
 package com.dominikdorn.rest.services;
 
-import com.dominikdorn.tuwien.evs.rest.annotations.Restful;
-import com.dominikdorn.tuwien.evs.rest.annotations.Searchable;
+import com.dominikdorn.rest.annotations.RestConverter;
+import com.dominikdorn.rest.annotations.Restful;
+import com.dominikdorn.rest.annotations.Searchable;
+import com.dominikdorn.rest.converter.Converter;
 import org.scannotation.AnnotationDB;
 
 import java.io.IOException;
@@ -19,40 +21,43 @@ import java.util.Set;
  */
 public class AnnotationScanner {
 
-    public Map<String, Class> getRegisteredObjectMap(URL classpath)
+    AnnotationDB db;
+
+
+    public void initialize(URL classpath)
     {
-        AnnotationDB db = new AnnotationDB();
+        db = new AnnotationDB();
         db.setScanClassAnnotations(true);
         db.setScanFieldAnnotations(true);
         db.setScanParameterAnnotations(false);
         db.setScanMethodAnnotations(false);
 
         db.addIgnoredPackages(
-                 "com.dominikdorn.tuwien.evs.rest.dao"
-                ,"com.dominikdorn.tuwien.evs.rest.services"
-                ,"com.dominikdorn.tuwien.evs.rest.annotations"
-                ,"com.dominikdorn.tuwien.evs.rest.listeners"
-                ,"com.dominikdorn.tuwien.evs.rest.requestHandling"
+                "com.dominikdorn.tuwien.evs.rest.dao"
+                , "com.dominikdorn.tuwien.evs.rest.services"
+                , "com.dominikdorn.tuwien.evs.rest.annotations"
+                , "com.dominikdorn.tuwien.evs.rest.listeners"
+                , "com.dominikdorn.tuwien.evs.rest.requestHandling"
         );
         try {
             db.scanArchives(new URL[]{classpath});
         } catch (IOException e) {
             throw new RuntimeException("failed to scan through the classpath. Is your harddrive ok?", e);
         }
+    }
 
+    public Map<String, Class> getRegisteredObjectMap() {
 
         Set<String> restfulClasses = db.getAnnotationIndex().get(Restful.class.getName());
 
         Map<String, Class> result = new HashMap<String, Class>();
 
-        if(restfulClasses != null)
-        {
-            for(String s : restfulClasses)
-            {
+        if (restfulClasses != null) {
+            for (String s : restfulClasses) {
                 try {
                     Class clazz = Class.forName(s);
                     Restful r = (Restful) clazz.getAnnotation(Restful.class);
-                    result.put(  r.value(),clazz  );
+                    result.put(r.value(), clazz);
                 } catch (ClassNotFoundException e) {
                     throw new RuntimeException("Couldn't load a class we previously found. This should never happen",
                             e);
@@ -64,18 +69,40 @@ public class AnnotationScanner {
     }
 
 
-    public Set<String> getSearchableFieldsForClass(Class clazz){
+    public Set<String> getSearchableFieldsForClass(Class clazz) {
         Set<String> result = new HashSet<String>();
 
-        for(Field f : clazz.getDeclaredFields())
-        {
+        for (Field f : clazz.getDeclaredFields()) {
             Searchable sa = f.getAnnotation(Searchable.class);
-            if(sa != null)
-            {
+            if (sa != null) {
                 result.add(f.getName());
             }
         }
         return result;
     }
+
+    public Map<Class, Converter> getConverters() {
+        Map<Class, Converter> result = new HashMap<Class, Converter>();
+        Set<String> classes = db.getAnnotationIndex().get(RestConverter.class.getName());
+
+        if (classes != null)
+            for (String s : classes) {
+                try {
+                    Class clazz = Class.forName(s);
+                    RestConverter r = (RestConverter) clazz.getAnnotation(RestConverter.class);
+                    result.put(r.forClass(), (Converter) clazz.newInstance());
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException("Couldn't load a class we previously found. This should never happen",
+                            e);
+                } catch (InstantiationException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+
+        return result;
+    }
+
 
 }
