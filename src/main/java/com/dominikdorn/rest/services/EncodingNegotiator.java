a016b6b77bb0c1f67373419b7f78ff4917924e78
Dominik Dorn <dominik@dominikdorn.com>
2010-04-09 01:07:40 +0200

    added EncodingNegotiator

---
+++ src/main/java/com/dominikdorn/rest/services/EncodingNegotiator.java
@@ -0,0 +1,28 @@
+package com.dominikdorn.rest.services;
+
+import com.dominikdorn.rest.services.OutputType;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public class EncodingNegotiator {
+
+    public OutputType detect(String contentType)
+    {
+        if(contentType == null)
+            return OutputType.UNSUPPORTED;
+
+        if(contentType.indexOf("application/xml") > -1)
+            return OutputType.XML;
+
+        if(contentType.indexOf("application/json") > -1)
+            return OutputType.JSON;
+
+        if(contentType.indexOf("application/x-www-form-urlencoded") > -1)
+            return OutputType.XHTML;
+
+        return OutputType.UNSUPPORTED;
+    }
+}
