3cc58fd680bab2fad726543e18988370e9205458
Dominik Dorn <dominik@dominikdorn.com>
2010-04-09 15:32:27 +0200

    completed @RestConverter annotation

--- src/main/java/com/dominikdorn/rest/annotations/RestConverter.java
+++ src/main/java/com/dominikdorn/rest/annotations/RestConverter.java
@@ -1,10 +1,17 @@
 package com.dominikdorn.rest.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Dominik Dorn
  * 0626165
  * dominik.dorn@tuwien.ac.at
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface RestConverter {
     Class forClass(); 
 }
