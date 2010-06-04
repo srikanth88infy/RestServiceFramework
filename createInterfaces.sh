0014ad0819c793175bfecd7bb945299d80b9ae75
Dominik Dorn <dominik@dominikdorn.com>
2010-06-04 02:11:23 +0200

    replaced hardcoded values with variables

--- createInterfaces.sh
+++ createInterfaces.sh
@@ -12,54 +12,54 @@ echo "EVS Rest 2010 Interface setup script. Run as root."
 echo "Creating internal IPs"
 for i in $LOCATION_NETWORKS; do 
 
-    echo "Creating Location: network 10.0.$i.0/24"
+    echo "Creating Location: network $PRIVATE_NW_PREFIX.$i.0/24"
 
-    echo "Creating Gateway 10.0.$i.$GATEWAY_IP "
+    echo "Creating Gateway $PRIVATE_NW_PREFIX.$i.$GATEWAY_IP "
 
-    ifconfig $IFACE:$currentIF 10.0.$i.$GATEWAY_IP/24 up
+    ifconfig $IFACE:$currentIF $PRIVATE_NW_PREFIX.$i.$GATEWAY_IP/24 up
 
     currentIF=$((currentIF+1))
 
-    echo "Creating LocationIndex 10.0.$i.$LOCATION_SERVER_IP "
+    echo "Creating LocationIndex $PRIVATE_NW_PREFIX.$i.$LOCATION_SERVER_IP "
 
-    ifconfig $IFACE:$currentIF 10.0.$i.$LOCATION_SERVER_IP/24 up
+    ifconfig $IFACE:$currentIF $PRIVATE_NW_PREFIX.$i.$LOCATION_SERVER_IP/24 up
     currentIF=$((currentIF+1))
 
 
-    echo "Creating storage interfaces for 10.0.$i.0/24"
+    echo "Creating storage interfaces for $PRIVATE_NW_PREFIX.$i.0/24"
 
-    echo "Creating 10.0.$i.$STORAGE_ONE_IP"
-    ifconfig $IFACE:$currentIF 10.0.$i.$STORAGE_ONE_IP/24 up
+    echo "Creating $PRIVATE_NW_PREFIX.$i.$STORAGE_ONE_IP"
+    ifconfig $IFACE:$currentIF $PRIVATE_NW_PREFIX.$i.$STORAGE_ONE_IP/24 up
     currentIF=$((currentIF+1))
 
-    echo "Creating 10.0.$i.$STORAGE_TWO_IP"
-    ifconfig $IFACE:$currentIF 10.0.$i.$STORAGE_TWO_IP/24 up
+    echo "Creating $PRIVATE_NW_PREFIX.$i.$STORAGE_TWO_IP"
+    ifconfig $IFACE:$currentIF $PRIVATE_NW_PREFIX.$i.$STORAGE_TWO_IP/24 up
     currentIF=$((currentIF+1))
 
-    echo "Creating 10.0.$i.$STORAGE_THREE_IP"
-    ifconfig $IFACE:$currentIF 10.0.$i.$STORAGE_THREE_IP/24 up
+    echo "Creating $PRIVATE_NW_PREFIX.$i.$STORAGE_THREE_IP"
+    ifconfig $IFACE:$currentIF $PRIVATE_NW_PREFIX.$i.$STORAGE_THREE_IP/24 up
     currentIF=$((currentIF+1))
 
 done
 
 echo "Creating external IPs for gateways"
 for i in $LOCATION_NETWORKS; do
-    echo "external ip for internal network GW 10.0.$i.$GATEWAY_IP is 193.82.$i.$GATEWAY_IP"
-    ifconfig $IFACE:$currentIF 193.82.$i.$GATEWAY_IP/24 up
+    echo "external ip for internal network GW $PRIVATE_NW_PREFIX.$i.$GATEWAY_IP is 193.82.$i.$GATEWAY_IP"
+    ifconfig $IFACE:$currentIF $PUBLIC_NW_PREFIX.$i.$GATEWAY_IP/24 up
     currentIF=$((currentIF+1))
 done;
 
 echo "Creating MetaService"
 
-echo "external ip for MetaService GW 10.0.$METASERVICE_NW.$GATEWAY_IP is 193.82.$METASERVICE_NW.$GATEWAY_IP"
-    ifconfig $IFACE:$currentIF 10.0.$METASERVICE_NW.$GATEWAY_IP/24 up
+echo "external ip for MetaService GW $PRIVATE_NW_PREFIX.$METASERVICE_NW.$GATEWAY_IP is $PUBLIC_NW_PREFIX.$METASERVICE_NW.$GATEWAY_IP"
+    ifconfig $IFACE:$currentIF $PRIVATE_NW_PREFIX.$METASERVICE_NW.$GATEWAY_IP/24 up
     currentIF=$((currentIF+1))
 
-    ifconfig $IFACE:$currentIF 193.82.$METASERVICE_NW.$GATEWAY_IP/24 up
+    ifconfig $IFACE:$currentIF $PUBLIC_NW_PREFIX.$METASERVICE_NW.$GATEWAY_IP/24 up
     currentIF=$((currentIF+1))
 
-echo "external ip for CentralIndex 10.0.$METASERVICE_NW.$LOCATION_SERVER_IP "
-    ifconfig $IFACE:$currentIF 10.0.$METASERVICE_NW.$LOCATION_SERVER_IP/24 up
+echo "external ip for CentralIndex $PRIVATE_NW_PREFIX.$METASERVICE_NW.$LOCATION_SERVER_IP "
+    ifconfig $IFACE:$currentIF $PRIVATE_NW_PREFIX.$METASERVICE_NW.$LOCATION_SERVER_IP/24 up
     currentIF=$((currentIF+1))
 
 
