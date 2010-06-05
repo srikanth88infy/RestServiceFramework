e3cfdb51854cfc8ae6e57572b3f1fcac77afcccd
Dominik Dorn <dominik@dominikdorn.com>
2010-06-05 14:19:00 +0200

    limiting memory usage for indexGateway

--- indexGateway/startIndexGateway.sh
+++ indexGateway/startIndexGateway.sh
@@ -16,4 +16,4 @@ fi
 NETWORK=$1
 
 echo MAVEN_OPTS=\"-Xms$META_SERVICE_GATEWAY_XMS -Xmx$META_SERVICE_GATEWAY_XMX -XX:PermSize=$META_SERVICE_GATEWAY_PERM\" mvn jetty:run-exploded -DmetaService.external.host=$META_SERVICE_ADDRESS -DmetaService.external.port=$META_SERVICE_PORT -DmetaService.internal.host=$PRIVATE_NW_PREFIX.$METASERVICE_NW.$GATEWAY_IP -DmetaService.internal.port=$META_SERVICE_PORT
-echo MAVEN_OPTS="-Xms$META_SERVICE_GATEWAY_XMS -Xmx$META_SERVICE_GATEWAY_XMX -XX:PermSize=$META_SERVICE_GATEWAY_PERM" mvn jetty:run-exploded -DmetaService.external.host=$META_SERVICE_ADDRESS -DmetaService.external.port=$META_SERVICE_PORT -DmetaService.internal.host=$PRIVATE_NW_PREFIX.$METASERVICE_NW.$GATEWAY_IP -DmetaService.internal.port=$META_SERVICE_PORT
+MAVEN_OPTS="-Xms$META_SERVICE_GATEWAY_XMS -Xmx$META_SERVICE_GATEWAY_XMX -XX:PermSize=$META_SERVICE_GATEWAY_PERM" mvn jetty:run-exploded -DmetaService.external.host=$META_SERVICE_ADDRESS -DmetaService.external.port=$META_SERVICE_PORT -DmetaService.internal.host=$PRIVATE_NW_PREFIX.$METASERVICE_NW.$GATEWAY_IP -DmetaService.internal.port=$META_SERVICE_PORT
