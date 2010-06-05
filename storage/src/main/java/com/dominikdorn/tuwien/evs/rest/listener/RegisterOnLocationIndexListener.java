03abdb4f10d6ac4d90140b25166a478c78151969
Dominik Dorn <dominik@dominikdorn.com>
2010-06-05 14:05:43 +0200

    renamed variables

--- storage/src/main/java/com/dominikdorn/tuwien/evs/rest/listener/RegisterOnLocationIndexListener.java
+++ storage/src/main/java/com/dominikdorn/tuwien/evs/rest/listener/RegisterOnLocationIndexListener.java
@@ -1,24 +1,11 @@
 package com.dominikdorn.tuwien.evs.rest.listener;
 
-import com.dominikdorn.rest.registration.RegisteringException;
 import com.dominikdorn.rest.registration.RegistrationService;
 import com.dominikdorn.rest.registration.RegistrationServiceImpl;
 import com.dominikdorn.rest.registration.RegistrationThread;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Dominik Dorn
@@ -28,8 +15,8 @@ import java.util.List;
 public class RegisterOnLocationIndexListener implements ServletContextListener{
     String ownAddress;
     String ownPort;
-    String locationIndexAddress;
-    String locationIndexPort;
+    String parentAddress;
+    String parentPort;
 
 
     @Override
@@ -37,8 +24,8 @@ public class RegisterOnLocationIndexListener implements ServletContextListener{
 
         ownAddress = (String) System.getProperties().get("storage.host");
         ownPort = (String) System.getProperties().get("storage.port");
-        locationIndexAddress = (String) System.getProperties().get("locationIndex.host");
-        locationIndexPort = (String) System.getProperties().get("locationIndex.port");
+        parentAddress = (String) System.getProperties().get("locationIndex.host");
+        parentPort = (String) System.getProperties().get("locationIndex.port");
 
         if(ownAddress == null)
             throw new RuntimeException("required System-Property storage.host not defined");
@@ -46,25 +33,25 @@ public class RegisterOnLocationIndexListener implements ServletContextListener{
         if(ownPort == null)
             throw new RuntimeException("required System-Property storage.port not defined");
 
-        if(locationIndexAddress == null)
+        if(parentAddress == null)
             throw new RuntimeException("required System-Property locationIndex.host not defined");
 
-        if(locationIndexPort == null)
+        if(parentPort == null)
             throw new RuntimeException("required System-Property locationIndex.port not defined");
 
         System.out.println("ownAddress = " + ownAddress);
         System.out.println("ownPort = " + ownAddress);
 
-        System.out.println("locationIndexAddress = " + locationIndexAddress);
-        System.out.println("locationIndexPort = " + locationIndexPort);
+        System.out.println("locationIndexAddress = " + parentAddress);
+        System.out.println("locationIndexPort = " + parentPort);
 
-        RegistrationThread thread = new RegistrationThread(2000, -1, locationIndexAddress, locationIndexPort, ownAddress, ownPort);
+        RegistrationThread thread = new RegistrationThread(2000, -1, parentAddress, parentPort, ownAddress, ownPort);
         thread.start();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         RegistrationService service = new RegistrationServiceImpl();
-        service.unRegisterOnParent(locationIndexAddress, locationIndexPort, ownAddress, ownPort);
+        service.unRegisterOnParent(parentAddress, parentPort, ownAddress, ownPort);
     }
 }
